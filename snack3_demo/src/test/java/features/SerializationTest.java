6d5b196494d31377fb6d0a760dc26906a5fb43d1
noear <noear@live.cn>
2019-11-22 17:44:12 +0800

    3.1.2.1

--- snack3_demo/src/test/java/features/SerializationTest.java
+++ snack3_demo/src/test/java/features/SerializationTest.java
@@ -6,6 +6,7 @@ import _models.UserGroupModel;
 import _models.UserModel;
 import org.junit.Test;
 import org.noear.snack.ONode;
+import org.noear.snack.core.TypeRef;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
@@ -101,6 +102,49 @@ public class SerializationTest {
 
         Object group23 = ONode.deserialize(json, null);
         assert group23 instanceof  Map;
+
+        assert group2.id == 9999;
+
+    }
+
+    @Test
+    public void test2_2() throws Exception {
+
+        UserGroupModel group = new UserGroupModel();
+        group.id = 9999;
+        group.users = new ArrayList<>();
+        group.users2 = new LinkedHashMap<>();
+        group.users3 = new TreeSet<>();
+        group.names = new String[5];
+        group.ids = new short[5];
+        group.iids = new Integer[5];
+        group.dd = new BigDecimal(12);
+        group.tt1 = new Timestamp(new Date().getTime());
+        group.tt2 = new Date();
+
+        for (short i = 0; i < 5 ; i++) {
+            UserModel user = new UserModel();
+            user.id = i;
+            user.name = "张三" + i;
+            user.note = null;
+            group.users.add(user);
+            group.users2.put(Integer.valueOf(i),user);
+            group.names[i] = "李四" + i;
+            group.ids[i] = i;
+        }
+
+        String json = ONode.stringify(group);//产生的json，没有@type
+        System.out.println(json);
+        UserGroupModel group2 = ONode.deserialize(json, UserGroupModel.class);
+
+        Object group22 = ONode.deserialize(json, (new TypeRef<UserGroupModel>(){}).getType());
+        assert group22 instanceof  UserGroupModel;
+
+        Object group23 = ONode.deserialize(json, LinkedHashMap.class);
+        assert group23 instanceof  Map;
+
+        Object group24 = ONode.deserialize(json, null);
+        assert group24 instanceof  Map;
 
         assert group2.id == 9999;
 
