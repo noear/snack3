cab4b5e5adc70c96918b101dae631e7af709e0b7
noear <noear@live.cn>
2019-11-22 17:17:00 +0800

    3.1.2.1

---
+++ snack3/src/main/java/org/noear/snack/core/TypeReference.java
@@ -0,0 +1,21 @@
+package org.noear.snack.core;
+
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+
+/**
+ *
+ * 例：(new TypeReference<List<String>>(){}).getType()
+ * */
+public abstract class TypeReference<T>{
+    protected final Type type;
+
+    public TypeReference() {
+        Type superClass = this.getClass().getGenericSuperclass();
+        this.type = ((ParameterizedType)superClass).getActualTypeArguments()[0];
+    }
+
+    public Type getType() {
+        return this.type;
+    }
+}
