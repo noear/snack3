aff2c39537abdc2756bc61537332bdab13080d1b
noear <noear@live.cn>
2019-11-27 15:49:30 +0800

    完善两个json path的demo

--- snack3_demo/src/test/java/demo/JsonPath.java
+++ snack3_demo/src/test/java/demo/JsonPath.java
@@ -9,26 +9,7 @@ import java.util.Map;
 public class JsonPath {
 @Test
 public void demo1() {
-    String json = "{ \"store\": {\n" +
-            "    \"book\": [ \n" +
-            "      { \"category\": \"reference\",\n" +
-            "        \"author\": \"Nigel Rees\",\n" +
-            "        \"title\": \"Sayings of the Century\",\n" +
-            "        \"price\": 8.95\n" +
-            "      },\n" +
-            "      { \"category\": \"fiction\",\n" +
-            "        \"author\": \"Evelyn Waugh\",\n" +
-            "        \"title\": \"Sword of Honour\",\n" +
-            "        \"price\": 12.99,\n" +
-            "        \"isbn\": \"0-553-21311-3\"\n" +
-            "      }\n" +
-            "    ],\n" +
-            "    \"bicycle\": {\n" +
-            "      \"color\": \"red\",\n" +
-            "      \"price\": 19.95\n" +
-            "    }\n" +
-            "  }\n" +
-            "}\n";
+    String json = "{\"store\":{\"book\":[{\"category\":\"reference\",\"author\":\"Nigel Rees\",\"title\":\"Sayings of the Century\",\"price\":8.95},{\"category\":\"fiction\",\"author\":\"Evelyn Waugh\",\"title\":\"Sword of Honour\",\"price\":12.99,\"isbn\":\"0-553-21311-3\"}],\"bicycle\":{\"color\":\"red\",\"price\":19.95}}}";
 
 
     ONode n = ONode.load(json);
