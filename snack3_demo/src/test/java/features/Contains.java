5e9adfb0fe6c9bd6ab0a9e04f9d48742d26298c7
noear <noear@live.cn>
2019-12-13 16:28:39 +0800

    3.1.5.13

--- snack3_demo/src/test/java/features/Contains.java
+++ snack3_demo/src/test/java/features/Contains.java
@@ -8,32 +8,63 @@ import java.util.Date;
 
 public class Contains {
     @Test
-    public void test1(){
-        JSONArray tmp = (JSONArray)JSONArray.parse("[1,2,3,4,5]");
-        assert  tmp.contains(2);
+    public void test0(){
 
-        tmp = (JSONArray)JSONArray.parse("[1,'2',3,4,5]");
-        assert  tmp.contains("2");
+        assert System.identityHashCode(null) == 0;
 
-        long times = System.currentTimeMillis();
-        Date time = new Date(times);
+        assert System.identityHashCode(0) != Integer.hashCode(0);
 
-        tmp = (JSONArray)JSONArray.parse("[1,'2',3,4,new Date("+times+")]");
-        assert  tmp.contains(time);
+        assert System.identityHashCode(0l) != Long.hashCode(0);
     }
 
     @Test
-    public void test2(){
-        ONode tmp = ONode.loadStr("[1,2,3,4,5]");
-        assert  tmp.contains(new ONode().val(2));
+    public void test1() {
+        JSONArray tmp = (JSONArray) JSONArray.parse("[1,2,3,4,5]");
+        assert tmp.contains(2);
 
-        tmp = ONode.loadStr("[1,'2',3,4,5]");
-        assert  tmp.contains(new ONode().val("2"));
+        tmp = (JSONArray) JSONArray.parse("[1,'2',3,4,5]");
+        assert tmp.contains("2");
 
         long times = System.currentTimeMillis();
         Date time = new Date(times);
 
-        tmp = ONode.loadStr("[1,'2',3,4,new Date("+times+")]");
-        assert  tmp.contains(new ONode().val(time));
+        tmp = (JSONArray) JSONArray.parse("[1,'2',3,4,new Date(" + times + ")]");
+        assert tmp.contains(time);
+    }
+
+    @Test
+    public void test21() {
+
+        assert  Long.hashCode(2) == new ONode().val(2).hashCode();
+
+        ONode tmp = ONode.loadStr("[1,2,3,4,5]");
+        assert tmp.ary().contains(new ONode().val(2));
+
+        tmp = ONode.loadStr("[1,'2',3,4,5]");
+        assert tmp.ary().contains(new ONode().val("2"));
+
+        long times = System.currentTimeMillis();
+        Date time = new Date(times);
+
+        tmp = ONode.loadStr("[1,'2',3,4,new Date(" + times + ")]");
+        assert tmp.ary().contains(new ONode().val(time));
+    }
+
+    @Test
+    public void test22() {
+
+        assert  Long.hashCode(2) == new ONode().val(2).hashCode();
+
+        ONode tmp = ONode.loadStr("[1,2,3,4,5]");
+        assert tmp.ary().contains(2l);
+
+        tmp = ONode.loadStr("[1,'2',3,4,5]");
+        assert tmp.ary().contains("2");
+
+        long times = System.currentTimeMillis();
+        Date time = new Date(times);
+
+        tmp = ONode.loadStr("[1,'2',3,4,new Date(" + times + ")]");
+        assert tmp.ary().contains(time);
     }
 }
