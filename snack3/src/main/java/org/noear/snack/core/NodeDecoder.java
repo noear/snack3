7c028ffb3eacac82b7ef3a82dd3a8ad132d7ab15
noear <noear@live.cn>
2021-01-01 20:56:14 +0800

    3.1.13

--- snack3/src/main/java/org/noear/snack/core/NodeDecoder.java
+++ snack3/src/main/java/org/noear/snack/core/NodeDecoder.java
@@ -3,7 +3,7 @@ package org.noear.snack.core;
 import org.noear.snack.ONode;
 
 /**
- * ONode 解码
+ * ONode 解码（用于控制自定义解码）
  *
  * @author noear 2021/1/1 created
  */
--- snack3/src/main/java/org/noear/snack/core/NodeEncoder.java
+++ snack3/src/main/java/org/noear/snack/core/NodeEncoder.java
@@ -3,7 +3,7 @@ package org.noear.snack.core;
 import org.noear.snack.ONode;
 
 /**
- * ONode 编码
+ * ONode 编码（用于自定义编码）
  *
  * @author noear 2021/1/1 created
  * */
