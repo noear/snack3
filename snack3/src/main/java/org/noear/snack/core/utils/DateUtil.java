eaecd12bbeac3376f21f8ce48bb2df0513b404d9
noear <noear@live.cn>
2021-06-14 00:18:12 +0800

    3.1.18

--- snack3/src/main/java/org/noear/snack/core/utils/DateUtil.java
+++ snack3/src/main/java/org/noear/snack/core/utils/DateUtil.java
@@ -11,52 +11,48 @@ import java.util.Date;
  * @author noear 2021/6/13 created
  */
 public class DateUtil {
-    public static final String FORMAT_24_ISO08601 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
-    public static final String FORMAT_19_ISO = "yyyy-MM-dd'T'HH:mm:ss";
-    public static final String FORMAT_19 = "yyyy-MM-dd HH:mm:ss";
-    public static final String FORMAT_22 = "yyyyMMddHHmmssSSSZ";//z: +0000
-    public static final String FORMAT_10 = "yyyy-MM-dd";
     public static final String FORMAT_29 = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX";
+    public static final String FORMAT_24_ISO08601 = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     public static final String FORMAT_23_a = "yyyy-MM-dd HH:mm:ss,SSS";
     public static final String FORMAT_23_b = "yyyy-MM-dd HH:mm:ss.SSS";
+    public static final String FORMAT_22 = "yyyyMMddHHmmssSSSZ";//z: +0000
+    public static final String FORMAT_19_ISO = "yyyy-MM-dd'T'HH:mm:ss";
+    public static final String FORMAT_19 = "yyyy-MM-dd HH:mm:ss";
+    public static final String FORMAT_10 = "yyyy-MM-dd";
+
+    public static final String FORMAT_8 = "HH:mm:ss";
 
     public static Date parse(String strVal) throws ParseException {
         final int len = strVal.length();
         String format = null;
-        if (len == 24) {
-            if (strVal.charAt(10) == 'T') {
-                format = FORMAT_24_ISO08601;
-            }
-        }
 
-        if (len == 22) {
-            format = FORMAT_22;
-        }
-
-        if (len == 19) {
-            if (strVal.charAt(10) == 'T') {
-                format = FORMAT_19_ISO;
-            } else {
-                format = FORMAT_19;
-            }
-        }
-
-        if (len == 10) {
-            format = FORMAT_10;
-        }
 
         if (len == 29) {
             if (strVal.charAt(26) == ':' && strVal.charAt(28) == '0') {
                 format = FORMAT_29;
             }
-        }
-
-        if (len == 23) {
+        } else if (len == 24) {
+            if (strVal.charAt(10) == 'T') {
+                format = FORMAT_24_ISO08601;
+            }
+        } else if (len == 23) {
             if (strVal.charAt(19) == ',') {
                 format = FORMAT_23_a;
             } else {
                 format = FORMAT_23_b;
             }
+        } else if (len == 22) {
+            format = FORMAT_22;
+        } else if (len == 19) {
+            if (strVal.charAt(10) == 'T') {
+                format = FORMAT_19_ISO;
+            } else {
+                format = FORMAT_19;
+            }
+        } else if (len == 10) {
+            format = FORMAT_10;
+        } else if (len == 8) {
+            format = FORMAT_8;
         }
 
         if (format != null) {
