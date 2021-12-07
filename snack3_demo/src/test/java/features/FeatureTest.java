a34bb9dc7cabc5d8fed18b9e2bbf0739fda6f48c
noear <noear@live.cn>
2021-12-07 17:54:41 +0800

    3.2.1

--- snack3_demo/src/test/java/features/FeatureTest.java
+++ snack3_demo/src/test/java/features/FeatureTest.java
@@ -1,14 +1,26 @@
 package features;
 
 import org.junit.Test;
-import org.noear.snack.core.Options;
+import org.noear.snack.ONode;
+import org.noear.snack.core.Feature;
 
 /**
  * @author noear 2021/12/7 created
  */
 public class FeatureTest {
     @Test
-    public void test(){
-        System.out.println(Options.def().getFeatures());
+    public void test() {
+        ONode oNode = new ONode();
+
+
+        System.out.println(oNode.options().getFeatures());
+        assert oNode.options().hasFeature(Feature.StringNullAsEmpty);
+
+
+        oNode.options().remove(Feature.StringNullAsEmpty);
+        System.out.println(oNode.options().getFeatures());
+        assert oNode.options().hasFeature(Feature.StringNullAsEmpty) == false;
+
+        assert oNode.get("name").getString() == null;
     }
 }
