a4cb39c274bc0a9f1efa1239a1d50874f2776690
noear <noear@live.cn>
2021-12-10 12:05:35 +0800

    3.2.2

---
+++ snack3_demo/src/test/java/_models/UserModel2.java
@@ -0,0 +1,26 @@
+package _models;
+
+import org.noear.snack.annotation.ONodeAttr;
+
+import java.util.Date;
+
+public class UserModel2 {
+    public int id;
+    @ONodeAttr(serialize = false)
+    public String name;
+    @ONodeAttr(deserialize = false)
+    public String note;
+
+    @ONodeAttr(format = "yyyyMMdd")
+    public Date date;
+
+    @Override
+    public String toString() {
+        return "UserModel2{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", note='" + note + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
