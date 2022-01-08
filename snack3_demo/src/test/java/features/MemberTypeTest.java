0302514afe54c1182629f810b5d6e81250a4e981
noear <noear@live.cn>
2022-01-08 11:51:13 +0800

    3.2.7

--- snack3_demo/src/test/java/features/MemberTypeTest.java
+++ snack3_demo/src/test/java/features/MemberTypeTest.java
@@ -7,7 +7,7 @@ import org.noear.snack.ONode;
 /**
  * @author noear 2021/12/31 created
  */
-public class TypeTest {
+public class MemberTypeTest {
     @Test
     public void test1(){
         String json = "{\"id\":1, \"name\":\"n\"}";
