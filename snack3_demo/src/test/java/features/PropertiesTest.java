117f386eeb12ce1276ef99baf5e3cde9384f5114
noear <noear@live.cn>
2022-02-20 16:21:47 +0800

    3.2.13

--- snack3_demo/src/test/java/features/PropertiesTest.java
+++ snack3_demo/src/test/java/features/PropertiesTest.java
@@ -22,13 +22,22 @@ public class PropertiesTest {
         ONode oNode = ONode.loadObj(props);
         String json = oNode.toJson();
 
-        System.out.println(oNode.toJson());
+        System.out.println(json);
 
-        Properties props2 = ONode.loadStr(oNode.toJson()).toObject(Properties.class);
+        Properties props2 = ONode.loadStr(json).toObject(Properties.class);
         String json2 = ONode.load(props2).toJson();
 
         System.out.println(json2);
 
         assert json.length() == json2.length();
+
+        Properties props3 = new Properties();
+        ONode.loadStr(json).bindTo(props3);
+        String json3 = ONode.load(props3).toJson();
+
+        System.out.println(json3);
+
+        assert json.length() == json3.length();
+
     }
 }
