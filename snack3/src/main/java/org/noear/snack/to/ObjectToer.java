e742e6f61171794a11f63926d3ccd9e3069689e7
noear <noear@live.cn>
2022-04-17 20:45:03 +0800

    3.2.21

--- snack3/src/main/java/org/noear/snack/to/ObjectToer.java
+++ snack3/src/main/java/org/noear/snack/to/ObjectToer.java
@@ -182,7 +182,7 @@ public class ObjectToer implements Toer {
         } else if (is(Object.class, clz)) {
             Object val = v.getRaw();
 
-            if (clz.isInterface() && val instanceof String) {
+            if (val instanceof String && clz.isInterface()) {
                 //如果是String，则约定为Class（上面已有String的识别）
                 Class<?> valClz = BeanUtil.loadClass((String) val);
                 return BeanUtil.newInstance(valClz);
