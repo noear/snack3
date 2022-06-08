1f07313b9d11b5858c7f4e0674ab1d03430df0c1
noear <noear@live.cn>
2022-06-08 12:45:05 +0800

    3.2.27

---
+++ snack3_demo/src/test/java/_models/UserModel3.java
@@ -0,0 +1,29 @@
+package _models;
+
+import org.noear.snack.annotation.ONodeAttr;
+import org.noear.snack.core.NodeEncoder;
+
+import java.util.Date;
+
+public class UserModel3 {
+    public NodeEncoder nodeEncoder;
+
+    public int id;
+    @ONodeAttr(ignore = true)
+    public String name;
+    @ONodeAttr(ignore = true)
+    public String note;
+
+    @ONodeAttr(format = "yyyyMMdd")
+    public Date date;
+
+    @Override
+    public String toString() {
+        return "UserModel2{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", note='" + note + '\'' +
+                ", date=" + date +
+                '}';
+    }
+}
---
+++ snack3_demo/src/test/java/features/AttrTest2.java
@@ -0,0 +1,40 @@
+package features;
+
+import _models.UserModel3;
+import org.junit.Test;
+import org.noear.snack.ONode;
+
+/**
+ * @author noear 2021/12/10 created
+ */
+public class AttrTest2 {
+    @Test
+    public void test() {
+        UserModel3 user = new UserModel3();
+
+        user.id = 1;
+        user.name = "noear";
+        user.note = "test";
+
+        String json = ONode.stringify(user);
+
+        System.out.println(json);
+
+        assert json.contains("noear") == false;
+        assert json.contains("test") == false;
+    }
+
+    @Test
+    public void test2() {
+        String json = "{id:1, name:'noear', note:'test'}";
+
+
+        UserModel3 user = ONode.deserialize(json, UserModel3.class);
+
+        System.out.println(json);
+
+        assert user.id == 1;
+        assert user.name == null;
+        assert user.note == null;
+    }
+}
