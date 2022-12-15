cd3e48c1f777092985cad54bf96f833115403803
noear <noear@live.cn>
2022-12-15 11:03:53 +0800

    3.2.50

---
+++ snack3_demo/src/test/java/features/NullTest.java
@@ -0,0 +1,22 @@
+package features;
+
+import org.junit.Test;
+import org.noear.snack.ONode;
+import org.noear.snack.core.Feature;
+
+/**
+ * @author noear 2022/12/15 created
+ */
+public class NullTest {
+    @Test
+    public void test1() {
+        String json = "{num:null}";
+        ONode node = ONode.load(json);
+
+        System.out.println(node.toJson());
+        assert node.get("num").isNull();
+
+        Object tmp = node.options(opt->opt.add(Feature.SerializeNulls)).toObject();
+        System.out.println(tmp);
+    }
+}
