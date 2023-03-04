09971ef5425bc6bed6cb34f125c7b94271bd7528
noear <noear@live.cn>
2023-03-04 09:04:33 +0800

    3.2.57: 增加 selectOrNew() 接口

--- snack3_demo/src/test/java/features/JsonPathNewTest.java
+++ snack3_demo/src/test/java/features/JsonPathNewTest.java
@@ -12,7 +12,17 @@ public class JsonPathNewTest {
         ONode oNode = new ONode();
         oNode.selectOrNew("$.orders[0].price").val(500);
         System.out.println(oNode.toJson());
+        //{"orders":[{"price":500}]}
 
         assert "{\"orders\":[{\"price\":500}]}".equals(oNode.toJson());
+
+
+        oNode.selectOrNew("$.orders[10].price").val(600);
+        System.out.println(oNode.toJson());
+        //{"orders":[{"price":500},null,null,null,null,null,null,null,null,null,{"price":600}]}
+
+        oNode.select("$.orders").forEach(n->n.asObject());
+        System.out.println(oNode.toJson());
+        //{"orders":[{"price":500},{},{},{},{},{},{},{},{},{},{"price":600}]}
     }
 }
